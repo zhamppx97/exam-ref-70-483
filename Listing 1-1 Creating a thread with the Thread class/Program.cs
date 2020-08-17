b2075ad2c7b5b2c60cef45a3b3a4050467cef4ad
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-17 11:28:08 +0700

    Add project files.

---
+++ Listing 1-1 Creating a thread with the Thread class/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Listing_1_1_Creating_a_thread_with_the_Thread_class
+{
+    class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+        public static void Main()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+            t.Join();
+        }
+    }
+}
