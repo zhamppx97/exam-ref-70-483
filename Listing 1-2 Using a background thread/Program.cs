39d169875b55c8b711ce08d7d199851c352360c8
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-17 12:37:00 +0700

    Add: Listing 1-2

---
+++ Listing 1-2 Using a background thread/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Listing_1_2_Using_a_background_thread
+{
+    class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void Main()
+        {
+            // If you run this application with the IsBackground property set to true,
+            // the application exits immediately. If you set it to false (create a foreground),
+            // the application prints the ThreadProc message ten times.
+            Thread t = new Thread(new ThreadStart(ThreadMethod))
+            {
+                IsBackground = true
+            };
+            t.Start();
+        }
+    }
+}
