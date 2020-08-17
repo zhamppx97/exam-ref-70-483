0d5f5a44def696e16104d320644bfcae67994e8c
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-17 14:36:48 +0700

    Add: Listing 1-3

---
+++ Listing 1-3 Using the ParameterizedThreadStart/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Listing_1_3_Using_the_ParameterizedThreadStart
+{
+    class Program
+    {
+        public static void ThreadMethod(object o)
+        {
+            for (int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+        public static void Main()
+        {
+            // In this case, the value 5 is passed to the ThreadMethod as an object.
+            // You can cast it to the expected type to use it in your method.
+            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));
+            t.Start(5);
+            t.Join();
+        }
+    }
+}
