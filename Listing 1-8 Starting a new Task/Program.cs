aaadae0120083f4f43ddfbb9b0a23ccbf0d370f7
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-19 11:29:53 +0700

    Add: Listing 1-8

---
+++ Listing 1-8 Starting a new Task/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Listing_1_8_Starting_a_new_Task
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Task t = Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.Write('*');
+                }
+            });
+            t.Wait();
+        }
+    }
+}
