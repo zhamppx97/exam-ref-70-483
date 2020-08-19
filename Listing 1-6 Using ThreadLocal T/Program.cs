dd94c72ca589004cde984d0faa21140b6d26ca28
Woraphon Khuarprakopkit <woraphon.k@mscsittipol.com>
2020-08-19 11:21:45 +0700

    Add: Listing 1-6

---
+++ Listing 1-6 Using ThreadLocal T/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Listing_1_6_Using_ThreadLocal_T
+{
+    class Program
+    {
+        public static ThreadLocal<int> _field = new ThreadLocal<int>(() => 
+        {
+            return Thread.CurrentThread.ManagedThreadId;
+        });
+
+        public static void Main()
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine("Thread A: {0}", i);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine("Thread B: {0}", i);
+                }
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
