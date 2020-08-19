2e6de26f8b70dd980ce4419651ca3c4108616916
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-19 15:14:23 +0700

    Add: Listing 1-11

---
+++ Listing 1-11 Scheduling different continuation tasks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Listing_1_11_Scheduling_different_continuation_tasks
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 97;
+            });
+            t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Canceled");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+            t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Faulted");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            var completedTask = t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Completed");
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            completedTask.Wait();
+        }
+    }
+}
