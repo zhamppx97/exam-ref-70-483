6bda2909dbd06f2ad44bf403787ea9b7431249c0
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-19 16:00:48 +0700

    Add: Listing 1-12

---
+++ Listing 1-12 Attaching child tasks to a parent task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Listing_1_12_Attaching_child_tasks_to_a_parent_task
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Task<Int32[]> parent = Task.Run(() =>
+            {
+                var results = new Int32[3];
+                new Task(() => results[0] = 0,
+                    TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => results[1] = 1,
+                    TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => results[2] = 2,
+                    TaskCreationOptions.AttachedToParent).Start();
+
+                return results;
+            });
+
+            var finalTask = parent.ContinueWith(
+                parentTask =>
+                {
+                    foreach (int i in parentTask.Result)
+                    {
+                        Console.WriteLine(i);
+                    }
+                });
+
+            finalTask.Wait();
+        }
+    }
+}
