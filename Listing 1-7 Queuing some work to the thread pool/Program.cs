68f0261906d2d8195a964a6ecf501418cf4c92b6
Woraphon Khuarprakopkit <woraphon.k@mscsittipol.com>
2020-08-19 11:25:48 +0700

    Add: Listing 1-7

---
+++ Listing 1-7 Queuing some work to the thread pool/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace Listing_1_7_Queuing_some_work_to_the_thread_pool
+{
+    class Program
+    {
+        public static void Main()
+        {
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                Console.WriteLine("Working on a thread from threadpool");
+            });
+            Console.ReadLine();
+        }
+    }
+}
