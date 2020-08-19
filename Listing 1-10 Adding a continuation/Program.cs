d5b5a12ced5948f8290180f84d16a6f156898c30
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-19 15:06:32 +0700

    Add: Listing 1-10

---
+++ Listing 1-10 Adding a continuation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Listing_1_10_Adding_a_continuation
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 97;
+            }).ContinueWith((i) =>
+            {
+                return i.Result * 2;
+            });
+            Console.WriteLine(t.Result);
+        }
+    }
+}
