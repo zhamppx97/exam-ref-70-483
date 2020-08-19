f854791f270fe0ba151badcc3e93725d859d4ca5
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-19 11:32:55 +0700

    Add: Listing 1-9

---
+++ Listing 1-9 Using a Task that returns a value/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Listing_1_9_Using_a_Task_that_returns_a_value
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 97;
+            });
+            Console.WriteLine(t.Result);
+        }
+    }
+}
