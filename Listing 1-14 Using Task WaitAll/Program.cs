e1d9a8a4daad24107d0dfaa18cc14f380a6d383e
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-20 13:30:46 +0700

    Add: Listing 1-14

---
+++ Listing 1-14 Using Task WaitAll/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Listing_1_14_Using_Task_WaitAll
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Task[] tasks = new Task[3];
+            tasks[0] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("1");
+                return 1;
+            });
+            tasks[1] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("2");
+                return 2;
+            });
+            tasks[2] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("3");
+                return 3;
+            });
+
+            Task.WaitAll(tasks);
+        }
+    }
+}
