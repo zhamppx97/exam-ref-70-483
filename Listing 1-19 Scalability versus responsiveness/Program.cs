ed267903e57079580ecd9a8728d99d2508ce9332
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-21 16:32:44 +0700

    Add: Listing 1-19

---
+++ Listing 1-19 Scalability versus responsiveness/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Listing_1_19_Scalability_versus_responsiveness
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static Task SleepAsyncA(int millisecondsTimeout)
+        {
+            return Task.Run(() => Thread.Sleep(millisecondsTimeout));
+        }
+
+        public static Task SleepAsyncB(int millisecondsTimeout)
+        {
+            TaskCompletionSource<bool> tcs = null;
+            var t = new Timer(delegate { tcs.TrySetResult(true); }, null, -1, -1);
+            tcs = new TaskCompletionSource<bool>(t);
+            t.Change(millisecondsTimeout, -1);
+            return tcs.Task;
+        }
+    }
+}
