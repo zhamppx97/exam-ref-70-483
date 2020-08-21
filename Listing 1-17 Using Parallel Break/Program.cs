a2b9504312bdcac53b395058f30e08e7fb1362bc
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-21 16:15:23 +0700

    Add: Listing 1-17

---
+++ Listing 1-17 Using Parallel Break/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Listing_1_17_Using_Parallel_Break
+{
+    class Program
+    {
+        public static void Main()
+        {
+            ParallelLoopResult result = Parallel.For(0, 1000, (int i, ParallelLoopState loopState) =>
+            {
+                if (i == 500)
+                {
+                    Console.WriteLine("Breaking loop");
+                    loopState.Break();
+                }
+                return;
+            });
+        }
+    }
+}
