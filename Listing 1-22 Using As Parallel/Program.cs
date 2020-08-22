8978a79355e9dd7ea2d7181a6c65171e53ad9dc9
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-22 19:35:50 +0700

    Add: Listing 1-22

---
+++ Listing 1-22 Using As Parallel/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Listing_1_22_Using_As_Parallel
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var numbers = Enumerable.Range(0, 10);
+            var parallelResult = numbers.AsParallel().Where(i => i % 2 == 0).ToArray();
+        }
+    }
+}
