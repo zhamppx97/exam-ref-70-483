f8740c45f54c8b9ebe5ecd6e11ffb9f7324a56a8
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-23 19:26:32 +0700

    Add: Listing 1-25

---
+++ Listing 1-25 Making a parallel query sequential/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Listing_1_25_Making_a_parallel_query_sequential
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var numbers = Enumerable.Range(0 ,20);
+            var parallelResult = numbers.AsParallel().AsOrdered().Where(i => i % 2 == 0).AsSequential();
+            foreach (int i in parallelResult.Take(5))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
