a94a4a143b8ab46ae7a4c2ec6bc578d2c149092b
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-23 19:18:40 +0700

    Add: Listing 1-23

---
+++ Listing 1-23 Unordered parallel query/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Listing_1_23_Unordered_parallel_query
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var numbers = Enumerable.Range(0, 10);
+            var parallelResult = numbers.AsParallel().Where(i => i % 2 == 0).ToArray();
+            foreach (int i in parallelResult)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
