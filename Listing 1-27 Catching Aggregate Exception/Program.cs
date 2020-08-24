1c1296926acb378c03c6a56b148b766d872ccb54
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-24 20:39:08 +0700

    Add: Listing 1-27

---
+++ Listing 1-27 Catching Aggregate Exception/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Listing_1_27_Catching_Aggregate_Exception
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var numbers = Enumerable.Range(0, 20);
+            try
+            {
+                var parallelResult = numbers.AsParallel().Where(i => IsEven(i));
+                parallelResult.ForAll(e => Console.WriteLine(e));
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("There where {0} exceptions", e.InnerExceptions.Count);
+            }
+        }
+
+        public static bool IsEven(int i)
+        {
+            if (i % 10 == 0) throw new ArgumentException("i");
+            return i % 2 == 0;
+        }
+    }
+}
