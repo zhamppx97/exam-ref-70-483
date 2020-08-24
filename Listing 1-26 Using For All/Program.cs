dfb47b091d636c74b7eb874753caafc673955ea9
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-24 20:23:35 +0700

    Add: Listing 1-26

---
+++ Listing 1-26 Using For All/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Listing_1_26_Using_For_All
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Enumerable.Range(0, 20);
+            var parallelResult = numbers.AsParallel().Where(i => i % 2 == 0);
+            parallelResult.ForAll(e => Console.WriteLine(e));
+        }
+    }
+}
