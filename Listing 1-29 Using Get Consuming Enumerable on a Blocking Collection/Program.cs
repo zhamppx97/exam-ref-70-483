57997c45032c8148c499fad122f0c1298680bc19
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-25 22:06:39 +0700

    Add: Listing 1-29

---
+++ Listing 1-29 Using Get Consuming Enumerable on a Blocking Collection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Listing_1_29_Using_Get_Consuming_Enumerable_on_a_Blocking_Collection
+{
+    class Program
+    {
+        static void Main()
+        {
+            BlockingCollection<string> col = new BlockingCollection<string>();
+            Task read = Task.Run(() =>
+            {
+                foreach (string v in col.GetConsumingEnumerable())
+                {
+                    Console.WriteLine(v);
+                }
+            });
+        }
+    }
+}
