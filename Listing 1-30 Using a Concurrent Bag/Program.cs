70a3069491070713f03a62674ce084983b696b79
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-25 22:15:23 +0700

    Add: Listing 1-30

---
+++ Listing 1-30 Using a Concurrent Bag/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Listing_1_30_Using_a_Concurrent_Bag
+{
+    class Program
+    {
+        static void Main()
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>
+            {
+                80, 90
+            };
+
+            int result;
+            if (bag.TryTake(out result))
+                Console.WriteLine(result);
+
+            if (bag.TryPeek(out result))
+                Console.WriteLine("There is a next item: {0}", result);
+        }
+    }
+}
