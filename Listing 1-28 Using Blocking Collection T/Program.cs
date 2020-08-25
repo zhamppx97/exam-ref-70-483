e41cd4920209af88667620486c7e9bb967e1a6c1
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-25 21:30:49 +0700

    Add: Listing 1-28

---
+++ Listing 1-28 Using Blocking Collection T/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Listing_1_28_Using_Blocking_Collection_T
+{
+    class Program
+    {
+        static void Main()
+        {
+            BlockingCollection<string> col = new BlockingCollection<string>();
+            Task read = Task.Run(() =>
+            {
+                while (true)
+                {
+                    Console.WriteLine(col.Take());
+                }
+            });
+
+            Task write = Task.Run(() =>
+            {
+                while (true)
+                {
+                    string s = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(s)) break;
+                    col.Add(s);
+                }
+            });
+
+            write.Wait();
+        }
+    }
+}
