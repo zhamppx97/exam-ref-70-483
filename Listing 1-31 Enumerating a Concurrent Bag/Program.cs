e7cb98461976457930c90ac2e5ac1fff2d2e4c85
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-26 15:44:33 +0700

    Add: Listing 1-31

---
+++ Listing 1-31 Enumerating a Concurrent Bag/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Listing_1_31_Enumerating_a_Concurrent_Bag
+{
+    class Program
+    {
+        static void Main()
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+            Task.Run(() =>
+            {
+                bag.Add(42);
+                Thread.Sleep(1000);
+                bag.Add(21);
+            });
+            Task.Run(() =>
+            {
+                foreach (int i in bag)
+                    Console.WriteLine(i);
+            }).Wait();
+        }
+    }
+}
