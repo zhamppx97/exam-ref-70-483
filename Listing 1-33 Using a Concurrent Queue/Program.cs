e4b9f4b08ec6a721c66e927691558117d02379c3
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-26 15:53:53 +0700

    Add: Listing 1-33

---
+++ Listing 1-33 Using a Concurrent Queue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Listing_1_33_Using_a_Concurrent_Queue
+{
+    class Program
+    {
+        static void Main()
+        {
+            ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+            queue.Enqueue(97);
+
+            if (queue.TryDequeue(out int result))
+                Console.WriteLine("Dequeued: {0}", result);
+        }
+    }
+}
