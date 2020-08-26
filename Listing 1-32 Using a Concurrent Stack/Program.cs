92b51bbb4c466abbd0719e27520d8d09d4b12768
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-26 15:50:54 +0700

    Add: Listing 1-32

---
+++ Listing 1-32 Using a Concurrent Stack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Listing_1_32_Using_a_Concurrent_Stack
+{
+    class Program
+    {
+        static void Main()
+        {
+            ConcurrentStack<int> stack = new ConcurrentStack<int>();
+            stack.Push(97);
+            
+            if (stack.TryPop(out int result))
+                Console.WriteLine("Popped: {0}", result);
+
+            stack.PushRange(new int[] { 1, 2, 3 });
+            int[] values = new int[2];
+            stack.TryPopRange(values);
+
+            foreach (int i in values)
+                Console.WriteLine(i);
+        }
+    }
+}
