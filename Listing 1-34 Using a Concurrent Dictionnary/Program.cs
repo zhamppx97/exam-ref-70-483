3633b72e8ef28c03c5ffefdf0772885b1f074c67
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-27 15:41:54 +0700

    Add: Listing 1-34

---
+++ Listing 1-34 Using a Concurrent Dictionnary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Listing_1_34_Using_a_Concurrent_Dictionnary
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dict = new ConcurrentDictionary<string, int>();
+            if (dict.TryAdd("k1", 42))
+            {
+                Console.WriteLine("Added");
+            }
+            if (dict.TryUpdate("k1", 21, 42))
+            {
+                Console.WriteLine("42 updated to 21");
+            }
+
+            dict["k1"] = 42;
+
+            int r1 = dict.AddOrUpdate("k1", 3, (s, i) => i * 2);
+            int r2 = dict.GetOrAdd("k2", 3);
+        }
+    }
+}
