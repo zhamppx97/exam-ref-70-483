afd049136d6f439296375ef954649631293746fc
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-27 15:51:06 +0700

    Add: Listing 1-35

---
+++ Listing 1-35 Accessing shared data in a multithreaded application/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Listing_1_35_Accessing_shared_data_in_a_multithreaded_application
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 0;
+            var up = Task.Run(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    n++;
+                }
+            });
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                n--;
+            }
+
+            up.Wait();
+            Console.WriteLine(n);
+        }
+    }
+}
