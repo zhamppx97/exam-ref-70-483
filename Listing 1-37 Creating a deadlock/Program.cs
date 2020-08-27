86eefc3f46b31bc87fc3a94f258535a8baa2919f
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-27 16:02:06 +0700

    Add: Listing 1-37

---
+++ Listing 1-37 Creating a deadlock/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Listing_1_37_Creating_a_deadlock
+{
+    class Program
+    {
+        static void Main()
+        {
+            object lockA = new object();
+            object lockB = new object();
+
+            var up = Task.Run(() =>
+            {
+                lock (lockA)
+                {
+                    Thread.Sleep(1000);
+                    lock (lockB)
+                    {
+                        Console.WriteLine("Locked A and B");
+                    }
+                }
+            });
+
+            lock (lockB)
+            {
+                lock (lockA)
+                {
+                    Console.WriteLine("Locked B and A");
+                }
+            }
+            up.Wait();
+        }
+    }
+}
