382a3967cd1d6bb2460d452a57543308fb7dc718
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-27 15:56:40 +0700

    Add: Listing 1-36

---
+++ Listing 1-36 Using the lock keyword/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Listing_1_36_Using_the_lock_keyword
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 0;
+            object _lock = new object();
+            var up = Task.Run(() =>
+             {
+                 for (int i = 0; i < 1000000; i++)
+                 {
+                     lock (_lock)
+                         n++;
+                 }
+             });
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                lock (_lock)
+                    n--;
+            }
+
+            up.Wait();
+            Console.WriteLine(n);
+        }
+    }
+}
