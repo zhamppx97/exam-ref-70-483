fcb0c85a18fa8f9527ffec48a6d85159f873e634
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-29 18:20:21 +0700

    Add; Listing 1-39

---
+++ Listing 1-39 A potential problem with multithreaded code/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Listing_1_39_A_potential_problem_with_multithreaded_code
+{
+    class Program
+    {
+        private static volatile int _flag = 0;
+        private static int _value = 0;
+
+        public static void Thread1()
+        {
+            _value = 5;
+            _flag = 1;
+        }
+
+        public static void Thread2()
+        {
+            if (_flag == 1)
+            {
+                Console.WriteLine(_value);
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
