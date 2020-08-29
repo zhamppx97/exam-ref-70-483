0aa3b31ab7b4f1230049e0fb41bcc6d010b67d36
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-29 18:15:37 +0700

    Add: Listing 1-38

---
+++ Listing 1-38 Generate code from a lock statement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Listing_1_38_Generate_code_from_a_lock_statement
+{
+    class Program
+    {
+        static void Main()
+        {
+            object gate = new object();
+            bool _lockTaken = false;
+            try
+            {
+                Monitor.Enter(gate, ref _lockTaken);
+            }
+            finally
+            {
+                if (_lockTaken)
+                    Monitor.Exit(gate);
+            }
+        }
+    }
+}
