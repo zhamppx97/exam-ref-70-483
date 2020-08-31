c035f45ad8d3bdfc89897c9e7c7fe51da565095a
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-01 00:28:21 +0700

    Add Listing 1-40

---
+++ Listing 1-40 Using the Interlocked class/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Listing_1_40_Using_the_Interlocked_class
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 0;
+            var up = Task.Run(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    Interlocked.Increment(ref n);
+                }
+            });
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Interlocked.Decrement(ref n);
+            }
+
+            up.Wait();
+            Console.WriteLine(n);
+        }
+    }
+}
