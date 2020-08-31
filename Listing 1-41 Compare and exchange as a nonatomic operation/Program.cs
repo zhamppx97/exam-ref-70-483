2b5b2034c0fe65650ffb534496809e714ebd4e5a
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-01 00:37:00 +0700

    Add Listing 1-41

---
+++ Listing 1-41 Compare and exchange as a nonatomic operation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Listing_1_41_Compare_and_exchange_as_a_nonatomic_operation
+{
+    class Program
+    {
+        static int value = 1;
+        static void Main()
+        {
+            Task t1 = Task.Run(() =>
+            {
+                if (value == 1)
+                {
+                    Thread.Sleep(1000);
+                    value = 2;
+                }
+            });
+
+            Task t2 = Task.Run(() =>
+             {
+                 value = 3;
+             });
+
+            Task.WaitAll(t1, t2);
+            Console.WriteLine(value);
+        }
+    }
+}
