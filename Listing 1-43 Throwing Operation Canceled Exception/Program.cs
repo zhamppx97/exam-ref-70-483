957ee3acc59a34d39ab731302bcf089f34eee2e9
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-02 22:36:54 +0700

    Add Listing 1-43

---
+++ Listing 1-43 Throwing Operation Canceled Exception/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Listing_1_43_Throwing_Operation_Canceled_Exception
+{
+    class Program
+    {
+        static void Main()
+        {
+            CancellationTokenSource cancellation = new CancellationTokenSource();
+            CancellationToken token = new CancellationToken();
+            Task task = Task.Run(() => 
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.Write("*");
+                    Thread.Sleep(1000);
+                }
+                token.ThrowIfCancellationRequested();
+            }, token);
+
+            try
+            {
+                Console.WriteLine("Press enter to stop the task");
+                Console.ReadLine();
+                cancellation.Cancel();
+                task.Wait();
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine(e.InnerExceptions[0].Message);
+            }
+            Console.WriteLine("Press enter to end the application");
+            Console.ReadLine();
+        }
+    }
+}
