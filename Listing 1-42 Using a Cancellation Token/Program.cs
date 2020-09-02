00353e08066744fdddffecb096f65f8a3c8c4555
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-02 22:16:09 +0700

    Add Listing 1-42

---
+++ Listing 1-42 Using a Cancellation Token/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Listing_1_42_Using_a_Cancellation_Token
+{
+    class Program
+    {
+        static void Main()
+        {
+            CancellationTokenSource cancellation = new CancellationTokenSource();
+            CancellationToken token = new CancellationToken();
+            Task task = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.Write("*");
+                    Thread.Sleep(1000);
+                }
+            }, token);
+
+            Console.WriteLine("Press enter to stop the task");
+            Console.ReadLine();
+            cancellation.Cancel();
+
+            Console.WriteLine("Press enter to end the application");
+            Console.ReadLine();
+        }
+    }
+}
