623384f1db5564a99127bbad5bc676868fc56e8f
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-08 11:49:45 +0700

    Add Listing 1-44

---
+++ Listing 1-44 Adding a continuation for canceled tasks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Listing_1_44_Adding_a_continuation_for_canceled_tasks
+{
+    class Program
+    {
+        static void Main()
+        {
+            CancellationTokenSource cancellation = new CancellationTokenSource();
+            CancellationToken token = new CancellationToken();
+            Task task = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.Write("*");
+                    Thread.Sleep(1000);
+                }
+
+                throw new OperationCanceledException();
+            }, token).ContinueWith((t) =>
+            {
+                t.Exception.Handle((e) => true);
+                Console.WriteLine("You have canceled the task");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+        }
+    }
+}
