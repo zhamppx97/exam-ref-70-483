419496ac06c79652a8cee49f3af206b235f01fe9
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-08 11:54:21 +0700

    Add Listing 1-45

---
+++ Listing 1-45 Setting a timeout on a task/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Listing_1_45_Setting_a_timeout_on_a_task
+{
+    class Program
+    {
+        static void Main()
+        {
+            Task longRunning = Task.Run(() =>
+            {
+                Thread.Sleep(10000);
+            });
+
+            int i = Task.WaitAny(new[] { longRunning }, 1000);
+
+            if (i == -1)
+            {
+                Console.WriteLine("Task timed out");
+            }
+        }
+    }
+}
