4424d3aefb3c53427c94891ba70f04a46900271b
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-10 15:44:52 +0700

    Add Listing 1-47

---
+++ Listing 1-47 Boolean OR operator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Listing_1_47_Boolean_OR_operator
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool x = true;
+            bool y = false;
+            bool result = x || y;
+            Console.WriteLine(result); // True
+        }
+    }
+}
