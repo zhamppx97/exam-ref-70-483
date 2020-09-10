333803a0d27a74db7ba10d2427f89c4440d34054
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-10 15:41:27 +0700

    Add Listing 1-46

---
+++ Listing 1-46 Using the equality operator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Listing_1_46_Using_the_equality_operator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 97;
+            int y = 0;
+            int z = 97;
+
+            Console.WriteLine(x == y); // False
+            Console.WriteLine(x == z); // True
+        }
+    }
+}
