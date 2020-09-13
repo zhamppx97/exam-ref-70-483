f8635e3c547d266269566b05dd42fe2535468ad7
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-13 21:03:59 +0700

    Add Listing 1-48

---
+++ Listing 1-48 Short-circuiting the OR operator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Listing_1_48_Short_circuiting_the_OR_operator
+{
+    class Program
+    {
+        public static void Main()
+        {
+            OrShortCircuit();
+        }
+
+        public static void OrShortCircuit()
+        {
+            bool x = true;
+            bool result = x || GetY();
+        }
+
+        private static bool GetY()
+        {
+            Console.WriteLine("This method doesn't get called");
+            return true;
+        }
+    }
+}
