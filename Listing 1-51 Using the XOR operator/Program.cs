7b42d8b4d626a23eeca101a77508968de4fe8412
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-14 17:55:15 +0700

    Add Listing 1-51

---
+++ Listing 1-51 Using the XOR operator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Listing_1_51_Using_the_XOR_operator
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(a ^ a);
+            Console.WriteLine(a ^ b);
+            Console.WriteLine(b ^ b);
+        }
+    }
+}
