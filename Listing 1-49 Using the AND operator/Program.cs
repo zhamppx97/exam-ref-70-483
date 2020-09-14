c98a42a3a4517eafe4959792e56fe5e901beda4b
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-14 17:53:03 +0700

    Add Listing 1-49, 1-50

---
+++ Listing 1-49 Using the AND operator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Listing_1_49_Using_the_AND_operator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int value = 97;
+            bool result = (0 < value) && (value < 100);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Listing 1-50 Short-circuiting the AND operator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Listing_1_50_Short_circuiting_the_AND_operator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Process("v Hello");
+        }
+
+        static void Process(string input)
+        {
+            bool result = (input != null) && (input.StartsWith("v"));
+            Console.WriteLine(result);
+        }
+    }
+}
