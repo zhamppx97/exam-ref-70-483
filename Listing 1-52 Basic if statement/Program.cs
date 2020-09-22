33d60f40b0b0602a34183a88f2c4db3bafcfe255
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-22 11:35:58 +0700

    Add Listing 1-52

---
+++ Listing 1-52 Basic if statement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Listing_1_52_Basic_if_statement
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool b = true;
+            if (b)
+                Console.WriteLine("True");
+        }
+    }
+}
