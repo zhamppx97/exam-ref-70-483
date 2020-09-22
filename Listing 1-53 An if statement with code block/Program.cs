afddf14e69aed95ebf8aca3e68508ddd1e560bcd
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-22 11:41:05 +0700

    Add Listing 1-53

---
+++ Listing 1-53 An if statement with code block/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Listing_1_53_An_if_statement_with_code_block
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool b = true;
+            if (b)
+            {
+                Console.WriteLine("Both these lines");
+                Console.WriteLine("Will be executed");
+            }
+        }
+    }
+}
