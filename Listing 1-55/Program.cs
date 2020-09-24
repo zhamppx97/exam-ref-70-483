fdde9dd9993986b396296979a3d604adbb73847a
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-24 11:11:35 +0700

    Add Listing 1-55

---
+++ Listing 1-55/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Listing_1_55_Using_an_else_statement
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool b = false;
+            if (b)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
