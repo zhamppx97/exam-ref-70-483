4998f7c078ac593108b3b6d4be061e163126da40
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-24 11:07:13 +0700

    Add Listing 1-54

---
+++ Listing 1-54 Code blocks and scoping/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Listing_1_54_Code_blocks_and_scoping
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool b = true;
+            if (b)
+            {
+                int r = 97;
+                b = false;
+            }
+        }
+    }
+}
