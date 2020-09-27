61f02e383b3e6b2a40cbef55f0afd50c930073df
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-27 21:58:19 +0700

    Add Listing 1-56

---
+++ Listing 1-56 Using multiple if else statements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Listing_1_56_Using_multiple_if_else_statements
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool b = false;
+            bool c = true;
+
+            if (b)
+            {
+                Console.WriteLine("b is true");
+            }
+            else if (c)
+            {
+                Console.WriteLine("c is true");
+            }
+            else
+            {
+                Console.WriteLine("b and c are false");
+            }
+        }
+    }
+}
