be97ad490ee1f1de149bf4a41b8b12230fadda86
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-27 22:06:21 +0700

    Add Listing 1-57

---
+++ Listing 1-57 A more readable nested if statement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Listing_1_57_A_more_readable_nested_if_statement
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool x = true;
+            bool y = true;
+
+            if (x)
+            {
+                if (y)
+                {
+                    F();
+                }
+                else
+                {
+                    G();
+                }
+            }
+        }
+
+        static void F()
+        {
+            Console.WriteLine("F");
+        }
+
+        static void G()
+        {
+            Console.WriteLine("G");
+        }
+    }
+}
