9794094fa9e7cac1d77dac60c3970bbdb08547cf
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-28 15:38:40 +0700

    Add Listing 1-60

---
+++ Listing 1-60 The conditional operator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Listing_1_60_The_conditional_operator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(GetValue(true));
+        }
+
+        static int GetValue(bool p)
+        {
+            if (p)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+            return p ? 1 : 0;
+        }
+    }
+}
