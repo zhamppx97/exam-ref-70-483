d1d703e67d6bb16714cb342983054f48302cfc7e
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-28 15:31:16 +0700

    Add Listing 1-58

---
+++ Listing 1-58 The null coalescing operator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Listing_1_58_The_null_coalescing_operator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int? x = null;
+            int y = x ?? -1;
+            Console.WriteLine(y);
+        }
+    }
+}
