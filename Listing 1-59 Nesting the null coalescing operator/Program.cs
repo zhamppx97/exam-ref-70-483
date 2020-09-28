c7e28782e9ad38c7e90e657306e2dff11bdf4c81
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-28 15:33:48 +0700

    Add Listing 1-59

---
+++ Listing 1-59 Nesting the null coalescing operator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Listing_1_59_Nesting_the_null_coalescing_operator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int? x = null;
+            int? z = null;
+            int y = x ?? z ?? -1;
+            Console.WriteLine(y);
+        }
+    }
+}
