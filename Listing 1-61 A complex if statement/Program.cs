2cb12c71a26b22d137dce7118bed34100d476e12
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-29 22:09:20 +0700

    Add Listing 1-61

---
+++ Listing 1-61 A complex if statement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Listing_1_61_A_complex_if_statement
+{
+    class Program
+    {
+        static void Main()
+        {
+            Check('a');
+        }
+
+        static void Check(char input)
+        {
+            if (input == 'a'
+                || input == 'e'
+                || input == 'i'
+                || input == 'o'
+                || input == 'u')
+            {
+                Console.WriteLine("Input is a vowel");
+            }
+            else
+            {
+                Console.WriteLine("Input is a consonant");
+            }
+        }
+    }
+}
