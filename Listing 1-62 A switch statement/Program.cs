de7758108b7072ec1a0014bba291a1fc7ed14d5a
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-29 22:16:06 +0700

    Add Listing 1-62

---
+++ Listing 1-62 A switch statement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Listing_1_62_A_switch_statement
+{
+    class Program
+    {
+        static void Main()
+        {
+            CheckWithSwitch('y');
+        }
+
+        static void CheckWithSwitch(char input)
+        {
+            switch (input)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    Console.WriteLine("Input is a vowel");
+                    break;
+                case 'y':
+                    Console.WriteLine("Input is sometimes a vowel");
+                    break;
+                default:
+                    Console.WriteLine("Input is a consonant");
+                    break;
+            }
+        }
+    }
+}
