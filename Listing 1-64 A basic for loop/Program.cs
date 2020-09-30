b2d59eaef998045e1f0b4c4503bbe413d5b983c6
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-30 16:13:28 +0700

    Add Listing 1-64

---
+++ Listing 1-64 A basic for loop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Listing_1_64_A_basic_for_loop
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine(values[i]);
+            }
+        }
+    }
+}
