a66c5aa1b73bbc586f965276ac21bb330e7d3a3a
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-09-30 16:09:50 +0700

    Add Listing 1-63

---
+++ Listing 1-63 goto in a switch statement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Listing_1_63_goto_in_a_switch_statement
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 1;
+            switch (i)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    goto case 2;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
