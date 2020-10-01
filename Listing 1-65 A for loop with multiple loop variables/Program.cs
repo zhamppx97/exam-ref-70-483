ea81e0296f3515c27c1932d3f1ba4176b7fbc2ce
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-01 14:35:17 +0700

    Add Listing 1-65

---
+++ Listing 1-65 A for loop with multiple loop variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Listing_1_65_A_for_loop_with_multiple_loop_variables
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int x = 0, y = values.Length - 1;
+                ((x < values.Length) && (y >= 0));
+                x++, y--)
+            {
+                Console.WriteLine(values[x]);
+                Console.WriteLine(values[y]);
+            }
+        }
+    }
+}
