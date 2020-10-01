0f9585f0935f60b232d0727b2817bdedd595f4c6
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-01 14:37:58 +0700

    Add Listing 1-66

---
+++ Listing 1-66 A for loop with a custom increment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Listing_1_66_A_for_loop_with_a_custom_increment
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < values.Length; i += 2)
+            {
+                Console.Write(values[i]);
+            }
+        }
+    }
+}
