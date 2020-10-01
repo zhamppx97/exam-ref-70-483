aaa9231da3500feb81a0465e4ec9811b2eef867b
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-01 14:43:55 +0700

    Add Listing 1-68

---
+++ Listing 1-68 A for loop with a continue statement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Listing_1_68_A_for_loop_with_a_continue_statement
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] == 4) continue;
+                Console.Write(values[i]);
+            }
+        }
+    }
+}
