e9e5dfd9a106a9ed41b5d482cc9471d6e0ecae11
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-01 14:40:43 +0700

    Add Listing 1-67

---
+++ Listing 1-67 A for loop with a break statement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Listing_1_67_A_for_loop_with_a_break_statement
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] == 4) break;
+                Console.Write(values[i]);
+            }
+        }
+    }
+}
