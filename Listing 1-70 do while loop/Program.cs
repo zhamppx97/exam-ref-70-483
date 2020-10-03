0dfae490d072cec8df691b663595bb7c81683042
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-03 22:56:47 +0700

    Add Listing 1-70

---
+++ Listing 1-70 do while loop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Listing_1_70_do_while_loop
+{
+    class Program
+    {
+        static void Main()
+        {
+            do
+            {
+                Console.WriteLine("Executed once!");
+            } while (true);
+        }
+    }
+}
