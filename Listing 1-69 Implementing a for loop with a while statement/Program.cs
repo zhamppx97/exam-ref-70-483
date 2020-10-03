484837407120d3e7a349028f5b355cac358d569d
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-03 22:54:33 +0700

    Add Listing 1-69

---
+++ Listing 1-69 Implementing a for loop with a while statement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Listing_1_69_Implementing_a_for_loop_with_a_while_statement
+{
+    class Program
+    {
+        static readonly int[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        static void Main()
+        {
+            int index = 0;
+            while (index < values.Length)
+            {
+                Console.WriteLine(values[index]);
+                index++;
+            }
+        }
+    }
+}
