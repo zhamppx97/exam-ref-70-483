18a349e2e9e9a3aedcc0eb89541b335c93675856
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-03 22:58:51 +0700

    Add Listing 1-71

---
+++ Listing 1-71 foreach loop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Listing_1_71_foreach_loop
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (var item in values)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
