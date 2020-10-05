15a58c3b7e8d2b8233fec4cca65980751369e0c8
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-05 15:28:13 +0700

    Add Listing 1-75

---
+++ Listing 1-75 Using a delegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Listing_1_75_Using_a_delegate
+{
+    class Program
+    {
+        public delegate int Calculate(int x, int y);
+        public static int Add(int x, int y) { return x + y; }
+        public static int Multiply(int x, int y) { return x * y; }
+
+        public static void UseDelegate()
+        {
+            Calculate calc = Add;
+            Console.WriteLine(calc(3, 4));
+
+            calc = Multiply;
+            Console.WriteLine(calc(3, 4));
+        }
+
+        static void Main()
+        {
+            UseDelegate();
+        }
+    }
+}
