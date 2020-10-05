3b00fdaa8b7d58b44b64c6950b2219216572ec5c
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-05 14:35:11 +0700

    Add Listing 1-72

---
+++ Listing 1-72 Changing items in a foreach/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listing_1_72_Changing_items_in_a_foreach
+{
+    class Program
+    {
+        class Person
+        {
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+        }
+
+        void CannotChangeForeachIterationVariable()
+        {
+            var people = new List<Person>
+            {
+                new Person() { Firstname = "John", Lastname = "Doe" },
+                new Person() { Firstname = "Jane", Lastname = "Doe" }
+            };
+
+            foreach (Person p in people)
+            {
+                p.Lastname = "Changed"; // This is allowed
+                // p = new Person(); // This gives a compile error
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
