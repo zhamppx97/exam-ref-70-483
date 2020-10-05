73e28adb25a4d77d6be2f4968f2ee86662d69149
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-05 15:21:47 +0700

    Add Listing 1-74

---
+++ Listing 1-74 goto statement with a label/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Listing_1_74_goto_statement_with_a_label
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 3;
+            if (x == 3) goto customLabel;
+            x++;
+
+            customLabel:
+            Console.WriteLine(x);
+        }
+    }
+}
