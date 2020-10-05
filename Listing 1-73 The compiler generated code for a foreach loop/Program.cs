9b7e195f42a0f271c3ff25b5ff8ed14f3a065347
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-05 15:14:15 +0700

    Add Listing 1-73

---
+++ Listing 1-73 The compiler generated code for a foreach loop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listing_1_73_The_compiler_generated_code_for_a_foreach_loop
+{
+    class Program
+    {
+        class Person
+        {
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+        }
+
+        static void Main()
+        {
+            var people = new List<Person>
+            {
+                new Person() { Firstname = "John", Lastname = "Doe" },
+                new Person() { Firstname = "Jane", Lastname = "Doe" }
+            };
+
+            List<Person>.Enumerator e = people.GetEnumerator();
+
+            try
+            {
+                Person v;
+                while (e.MoveNext())
+                {
+                    v = e.Current;
+                }
+            }
+            finally
+            {
+                System.IDisposable d = e as System.IDisposable;
+                if (d != null) d.Dispose();
+            }
+        }
+    }
+}
