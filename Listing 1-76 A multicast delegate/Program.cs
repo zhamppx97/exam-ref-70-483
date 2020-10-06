be74a23d9914099d04b9bf057c225b568af3887f
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-06 11:13:02 +0700

    Add Listing 1-76

---
+++ Listing 1-76 A multicast delegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Listing_1_76_A_multicast_delegate
+{
+    class Program
+    {
+        public static void MethodOne()
+        {
+            Console.WriteLine("MethodOne");
+        }
+
+        public static void MethodTwo()
+        {
+            Console.WriteLine("MethodTwo");
+        }
+
+        public delegate void Del();
+
+        public static void Multicast()
+        {
+            Del d = MethodOne;
+            d += MethodTwo;
+            d();
+        }
+
+        static void Main()
+        {
+            Multicast();
+        }
+    }
+}
