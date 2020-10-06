846311ee7a2057dfce4cd23c75ff4b2153c30a46
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-06 11:45:46 +0700

    Add Listing 1-80

---
+++ Listing 1-80 Creating a lambda expression with multiple statements/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Listing_1_80_Creating_a_lambda_expression_with_multiple_statements
+{
+    class Program
+    {
+        public delegate int Calculate(int x, int y);
+        static void Main()
+        {
+            Calculate calc = (x, y) =>
+            {
+                Console.WriteLine("Adding numbers");
+                return x + y;
+            };
+            int result = calc(3, 4);
+        }
+    }
+}
