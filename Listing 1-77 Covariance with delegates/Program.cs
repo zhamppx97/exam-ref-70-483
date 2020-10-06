bf59d361d2607317589944cfd7f743885e4d91a3
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-06 11:19:51 +0700

    Add Listing 1-77

---
+++ Listing 1-77 Covariance with delegates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Listing_1_77_Covariance_with_delegates
+{
+    class Program
+    {
+        public delegate TextWriter CovarianceDel();
+        public static StreamWriter MethodStream() { return null; }
+        public static StringWriter MethodString() { return null; }
+
+        static void Main()
+        {
+            CovarianceDel del;
+            del = MethodStream;
+            del = MethodString;
+        }
+    }
+}
