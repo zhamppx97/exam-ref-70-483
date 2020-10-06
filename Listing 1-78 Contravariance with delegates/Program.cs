9a1b124b3dc01a25e5484dd7222fa12f5a459527
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-06 11:29:41 +0700

    Add Listing 1-78

---
+++ Listing 1-78 Contravariance with delegates/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Listing_1_78_Contravariance_with_delegates
+{
+    class Program
+    {
+        public static void DoSomething(TextWriter tw) { }
+        public delegate void ContravarianceDel(StreamWriter sw);
+        
+        public static void Main()
+        {
+            ContravarianceDel del = DoSomething;
+        }
+    }
+}
