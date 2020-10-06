10d8ce3094a0039594b9e482fe5177b2c9773bfa
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-06 11:39:51 +0700

    Add Listing 1-79

---
+++ Listing 1-79 Lambda expression to create a delegate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Listing_1_79_Lambda_expression_to_create_a_delegate
+{
+    class Program
+    {
+        public delegate int Calculate(int x, int y);
+        static void Main()
+        {
+            Calculate calc = (x, y) => x + y;
+            Console.WriteLine(calc(3, 4));
+            calc = (x, y) => x * y;
+            Console.WriteLine(calc(3, 4));
+        }
+    }
+}
