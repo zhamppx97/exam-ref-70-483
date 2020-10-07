b3bb340af7d72fe2b03ef8971d5d11b0b10d815e
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-07 12:02:02 +0700

    Add Listing 1-81

---
+++ Listing 1-81 Using the Action delegate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Listing_1_81_Using_the_Action_delegate
+{
+    class Program
+    {
+        static void Main()
+        {
+            Action<int, int> calc = (x, y) =>
+            {
+                Console.WriteLine(x + y);
+            };
+            calc(3, 4);
+        }
+    }
+}
