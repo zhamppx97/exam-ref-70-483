287dc70dc7d5effec00e50cd650235a3bbdfbab6
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-07 12:12:50 +0700

    Add Listing 1-83

---
+++ Listing 1-83 Using the event keyword/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Listing_1_83_Using_the_event_keyword
+{
+    public class Pub
+    {
+        public event Action OnChange = delegate { };
+
+        public void Raise()
+        {
+            OnChange();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Pub p = new Pub();
+            p.OnChange += () => Console.WriteLine("Event raised to method 1");
+            p.OnChange += () => Console.WriteLine("Event raised to method 2");
+            p.Raise();
+        }
+    }
+}
