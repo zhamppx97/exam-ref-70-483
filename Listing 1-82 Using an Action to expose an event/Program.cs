c293c467ac1dfb7b7a8c5ea9cb209ae2f30f7494
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-07 12:08:38 +0700

    Add Listing 1-82

---
+++ Listing 1-82 Using an Action to expose an event/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Listing_1_82_Using_an_Action_to_expose_an_event
+{
+    public class Pub
+    {
+        public Action OnChange { get; set; }
+
+        public void Raise()
+        {
+            if (OnChange != null)
+            {
+                OnChange();
+            }
+        }
+    }
+
+    class Program
+    {
+        public static void CreateAndRaise()
+        {
+            Pub p = new Pub();
+            p.OnChange += () => Console.WriteLine("Event raised to method 1");
+            p.OnChange += () => Console.WriteLine("Event raised to method 2");
+            p.Raise();
+        }
+
+        static void Main()
+        {
+            CreateAndRaise();
+        }
+    }
+}
