e4d7f42df8b7a1b8662f7b4c652e9d2109ac4e8f
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-12 16:20:55 +0700

    Add Listing 1-85

---
+++ Listing 1-85 Custom event accessor/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Listing_1_85_Custom_event_accessor
+{
+    class Program
+    {
+        public class MyArgs : EventArgs
+        {
+            public int _value { get; set; }
+            public MyArgs(int value)
+            {
+                _value = value;
+            }
+        }
+
+        public class Pub
+        {
+            private event EventHandler<MyArgs> onChange = delegate { };
+
+            public event EventHandler<MyArgs> OnChange
+            {
+                add
+                {
+                    lock (onChange)
+                    {
+                        onChange += value;
+                    }
+                }
+                remove
+                {
+                    lock (onChange)
+                    {
+                        onChange -= value;
+                    }
+                }
+            }
+
+            public void Raise()
+            {
+                onChange(this, new MyArgs(97));
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
