68492abccc8ee1de2bcd819a877693d6eb576ee5
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-12 16:10:55 +0700

    Add Listing 1-84

---
+++ Listing 1-84 Custom event arguments/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Listing_1_84_Custom_event_arguments
+{
+    class Program
+    {
+        public class MyArgs : EventArgs
+        {
+            public int _value { get; set; }
+            public MyArgs(int value)
+            {
+                _value = value;
+            }
+        }
+
+        public class Pub
+        {
+            public event EventHandler<MyArgs> OnChange = delegate { };
+            public void Raise()
+            {
+                OnChange(this, new MyArgs(97));
+            }
+        }
+
+        public static void CreateAndRaise()
+        {
+            Pub p = new Pub();
+            p.OnChange += (sender, e) => Console.WriteLine("Event raised: {0}", e._value);
+            p.Raise();
+        }
+
+        static void Main()
+        {
+            CreateAndRaise();
+        }
+    }
+}
