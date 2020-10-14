79b5559779de45be459c3d563c4bfdb0d5cb29c8
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-14 17:20:29 +0700

    Add Listing 1-91

---
+++ Listing 1-91 Using a finally block/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Listing_1_91_Using_a_finally_block
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s = Console.ReadLine();
+            try
+            {
+                int i = int.Parse(s);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("You need to enter a value");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("{0} is not a valid number. Please try again", s);
+            }
+            finally
+            {
+                Console.WriteLine("Program complete.");
+            }
+        }
+    }
+}
