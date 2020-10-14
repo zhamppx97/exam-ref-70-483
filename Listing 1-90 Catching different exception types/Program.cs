b92cb94854ec9ac386ab33cb514993165aa7b257
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-14 17:07:29 +0700

    Add Listing 1-90

---
+++ Listing 1-90 Catching different exception types/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Listing_1_90_Catching_different_exception_types
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int i = int.Parse("s");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("You need to enter a value");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("{0} is not a valid number. Please try again", "s");
+            }
+        }
+    }
+}
