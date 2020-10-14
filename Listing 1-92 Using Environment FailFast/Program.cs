a58aea2b21f8bf003ac6ce41debbb62acd145d43
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-14 18:00:13 +0700

    Add Listing 1-92

---
+++ Listing 1-92 Using Environment FailFast/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Listing_1_92_Using_Environment_FailFast
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s = Console.ReadLine();
+            try
+            {
+                int i = int.Parse(s);
+                if (i == 97) Environment.FailFast("Special number entered");
+            }
+            finally
+            {
+                Console.WriteLine("Program complete.");
+            }
+        }
+    }
+}
