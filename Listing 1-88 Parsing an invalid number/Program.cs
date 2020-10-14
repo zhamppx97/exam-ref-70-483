25469c2a4b59db55735f4780e4f2627614d83fac
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-14 11:48:09 +0700

    Add Listing 1-88

---
+++ Listing 1-88 Parsing an invalid number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Listing_1_88_Parsing_an_invalid_number
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s = "NaN";
+            int i = int.Parse(s);
+
+            // Error
+            // Input string was not in a correct format.
+        }
+    }
+}
