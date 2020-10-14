118abc3f93b50b951a1c3bc25b5865fe06ec1d0e
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-14 11:52:21 +0700

    Add Listing 1-89

---
+++ Listing 1-89 Catching a FormatException/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Listing_1_89_Catching_a_FormatException
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string s = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(s)) break;
+                try
+                {
+                    int i = int.Parse(s);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("{0} is not a valid number. Please try again", s);
+                }
+            }
+        }
+    }
+}
