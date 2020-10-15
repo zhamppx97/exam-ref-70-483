9568ea4ae5d688594e15133da6b9375a88729b20
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-15 11:16:10 +0700

    Add Listing 1-94

---
+++ Listing 1-94 Throwing an ArgumentNullException/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Listing_1_94_Throwing_an_ArgumentNullException
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static string OpenAndParse(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentException("fileName", "Filename is required");
+
+            return File.ReadAllText(fileName);
+        }
+    }
+}
