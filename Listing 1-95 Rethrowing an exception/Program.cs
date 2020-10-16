945a108ada0251ab9c08385365db5373e48f508b
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-16 16:25:14 +0700

    Add Listing 1-95

---
+++ Listing 1-95 Rethrowing an exception/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Listing_1_95_Rethrowing_an_exception
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int x = int.Parse("s");
+            }
+            catch (Exception logEx)
+            {
+                Log(logEx);
+                throw;
+            }
+        }
+
+        static void Log(Exception logEx)
+        {
+            Console.WriteLine(logEx);
+        }
+    }
+}
