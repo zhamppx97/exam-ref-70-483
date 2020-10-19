840ab71467d6b1e5d6e6e0ad552d4c49fb8d4e30
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-19 11:28:04 +0700

    Add Listing 1-97

---
+++ Listing 1-97 Using Exception Dispatchinfo Throw/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace Listing_1_97_Using_Exception_Dispatchinfo_Throw
+{
+    class Program
+    {
+        static void Main()
+        {
+            ExceptionDispatchInfo possibleException = null;
+
+            try
+            {
+                string s = Console.ReadLine();
+                int.Parse(s);
+            }
+            catch (FormatException e)
+            {
+                possibleException = ExceptionDispatchInfo.Capture(e);
+            }
+
+            if (possibleException != null)
+            {
+                possibleException.Throw();
+            }
+        }
+    }
+}
