709f9f255518c5d6f4b4f128bb1742d9d6452f64
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-10-19 11:43:34 +0700

    Add Listing 1-98

---
+++ Listing 1-98 Creating a custom exception/OrderProcessingException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Listing_1_98_Creating_a_custom_exception
+{
+    [Serializable]
+    public class OrderProcessingException : Exception, ISerializable
+    {
+        public int OrderId { get; private set; }
+        public OrderProcessingException(int orderId)
+        {
+            OrderId = orderId;
+            this.HelpLink = "https://www.example.com/infoaboutexception";
+        }
+
+        public OrderProcessingException(int orderId, string message, Exception innerException) : base(message, innerException)
+        {
+            OrderId = orderId;
+            this.HelpLink = "https://www.example.com/infoaboutexception";
+        }
+
+        protected OrderProcessingException(SerializationInfo info, StreamingContext context)
+        {
+            OrderId = (int)info.GetValue("OrderId", typeof(int));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("OrderId", OrderId, typeof(int));
+        }
+    }
+}
---
+++ Listing 1-98 Creating a custom exception/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Listing_1_98_Creating_a_custom_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
